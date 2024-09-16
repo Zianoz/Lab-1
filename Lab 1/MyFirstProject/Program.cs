d8e768b555726ee830f4bb6ca9b1ef7e7aac517d
Zianoz <87281912+Zianoz@users.noreply.github.com>
2024-09-16 13:16:05 +0200

    a

--- Lab 1/MyFirstProject/Program.cs
+++ Lab 1/MyFirstProject/Program.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
+//Pakapol (Zian) Pathumthammarong NET24
 namespace MyFirstProject
 {
     internal class Program
